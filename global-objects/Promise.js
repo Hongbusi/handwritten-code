45e1d97c97285e0887ec6bfb2595b12ea9bbf2a3
Hongbusi <q951686662@qq.com>
2022-04-17 14:46:50 +0800

    chore: promise

--- global-objects/Promise.js
+++ global-objects/Promise.js
@@ -4,6 +4,17 @@ const PROMISE_STATUS_PENDING = 'pending'
 const PROMISE_STATUS_FULFILLED = 'fulfilled'
 const PROMISE_STATUS_REJECTED = 'rejected'
 
+// 工具函数
+function execFunctionWithCatchError(execFn, value, resolve, reject) {
+  try {
+    const result = execFn(value)
+    resolve(result)
+  }
+  catch (err) {
+    reject(err)
+  }
+}
+
 class HbsPromise {
   constructor(executor) {
     this.status = PROMISE_STATUS_PENDING
@@ -20,7 +31,7 @@ class HbsPromise {
           this.status = PROMISE_STATUS_FULFILLED
           this.value = value
           this.onFulfilledFns.forEach((fn) => {
-            fn(this.value)
+            fn()
           })
         })
       }
@@ -34,54 +45,59 @@ class HbsPromise {
           this.status = PROMISE_STATUS_REJECTED
           this.reason = reason
           this.onRejectedFns.forEach((fn) => {
-            fn(this.reason)
+            fn()
           })
         })
       }
     }
 
-    executor(resolve, reject)
+    try {
+      executor(resolve, reject)
+    }
+    catch (err) {
+      reject(err)
+    }
   }
 
   then(onFulfilled, onRejected) {
-    // 1. 如果在 then 调用的时候，状态已经确定下来
-    if (this.status === PROMISE_STATUS_FULFILLED && onFulfilled) {
-      onFulfilled(this.value)
-    }
+    return new HbsPromise((resolve, reject) => {
+      // 1. 如果在 then 调用的时候，状态已经确定下来
+      if (this.status === PROMISE_STATUS_FULFILLED && onFulfilled) {
+        execFunctionWithCatchError(onFulfilled, this.value, resolve, reject)
+      }
 
-    if (this.status === PROMISE_STATUS_REJECTED && onRejected) {
-      onRejected(this.reason)
-    }
+      if (this.status === PROMISE_STATUS_REJECTED && onRejected) {
+        execFunctionWithCatchError(onRejected, this.reason, resolve, reject)
+      }
 
-    // 2. 将成功回调和失败回调放到数组中
-    if (this.status === PROMISE_STATUS_PENDING) {
-      this.onFulfilledFns.push(onFulfilled)
-      this.onRejectedFns.push(onRejected)
-    }
+      // 2. 将成功回调和失败回调放到数组中
+      if (this.status === PROMISE_STATUS_PENDING) {
+        this.onFulfilledFns.push(() => {
+          execFunctionWithCatchError(onFulfilled, this.value, resolve, reject)
+        })
+        this.onRejectedFns.push(() => {
+          execFunctionWithCatchError(onRejected, this.reason, resolve, reject)
+        })
+      }
+    })
   }
 }
 
 const promise = new HbsPromise((resolve, reject) => {
   resolve('resolve')
-  reject('reject')
+  // reject('reject')
+  // throw new Error('err message')
 })
 
 promise.then((res) => {
-  console.log('res: ', res)
+  console.log('res1: ', res)
+  // return '111'
+  throw new Error('err message')
 }, (err) => {
-  console.log('err: ', err)
-})
-
-promise.then((res) => {
+  console.log('err1: ', err)
+  throw new Error('err message')
+}).then((res) => {
   console.log('res2: ', res)
 }, (err) => {
   console.log('err2: ', err)
 })
-
-setTimeout(() => {
-  promise.then((res) => {
-    console.log('res3: ', res)
-  }, (err) => {
-    console.log('err3: ', err)
-  })
-}, 1000)
